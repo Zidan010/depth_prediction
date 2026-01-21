f782ac309c6e3fecb4a2ddf961c21c30c7fbd6d7
Tasfiq-K <tasfiq.kamran@gmail.com>
2026-01-21 16:58:09 +0600

    --added MobilenetEncoder in __init__

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,4 +1,5 @@
 from .resnet_encoder import ResnetEncoder
+from .mobilenet_encoder import MobileNetEncoder
 from .depth_decoder import DepthDecoder
 from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
