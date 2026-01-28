7b269ad9da3836ec0e817623ffea66fd14cbd543
Tasfiq-K <tasfiq.kamran@gmail.com>
2026-01-28 15:23:55 +0600

    --a mobile awared depth decoder script added and also initialized in __init__ for import

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,5 +1,6 @@
 from .resnet_encoder import ResnetEncoder
 from .mobilenet_encoder import MobileNetEncoder
 from .depth_decoder import DepthDecoder
+from .mobile_depth_decoder import MobileDepthDecoder
 from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
